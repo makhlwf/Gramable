d51da0d9b9e571491a5b93ea1b83e67baf87396d
makhlwf <altrhwnyashrf1@gmail.com>
2026-09-07 16:23:02 +0200

    feat(ci): add release workflow with in-app update dialog integration and secrets

--- TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
+++ TMessagesProj_App/src/main/java/org/telegram/messenger/ApplicationLoaderImpl.java
@@ -1,10 +1,137 @@
 package org.telegram.messenger;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.text.TextUtils;
+import android.view.ViewGroup;
+
+import androidx.core.content.FileProvider;
+
 import org.telegram.messenger.regular.BuildConfig;
+import org.telegram.tgnet.TLRPC;
+import org.telegram.ui.Components.AlertsCreator;
+import org.telegram.ui.Components.UpdateAppAlertDialog;
+import org.telegram.ui.Components.UpdateLayout;
+import org.telegram.ui.IUpdateLayout;
+
+import java.io.File;
 
 public class ApplicationLoaderImpl extends ApplicationLoader {
     @Override
     protected String onGetApplicationId() {
         return BuildConfig.APPLICATION_ID;
     }
+
+    @Override
+    protected boolean isStandalone() {
+        return true;
+    }
+
+    @Override
+    protected boolean isBeta() {
+        return BuildConfig.DEBUG || BuildConfig.APPLICATION_ID.endsWith(".beta");
+    }
+
+    @Override
+    public boolean isCustomUpdate() {
+        return !TextUtils.isEmpty(org.telegram.messenger.BuildConfig.BETA_URL);
+    }
+
+    @Override
+    public BetaUpdate getUpdate() {
+        if (!isCustomUpdate()) return null;
+        return BetaUpdaterController.getInstance().getUpdate();
+    }
+
+    @Override
+    public void checkUpdate(boolean force, Runnable whenDone) {
+        if (!isCustomUpdate()) return;
+        BetaUpdaterController.getInstance().checkForUpdate(force, whenDone);
+    }
+
+    @Override
+    public void downloadUpdate() {
+        if (!isCustomUpdate()) return;
+        BetaUpdaterController.getInstance().downloadUpdate();
+    }
+
+    @Override
+    public void cancelDownloadingUpdate() {
+        if (!isCustomUpdate()) return;
+        BetaUpdaterController.getInstance().cancelDownloadingUpdate();
+    }
+
+    @Override
+    public boolean isDownloadingUpdate() {
+        if (!isCustomUpdate()) return false;
+        return BetaUpdaterController.getInstance().isDownloading();
+    }
+
+    @Override
+    public float getDownloadingUpdateProgress() {
+        if (!isCustomUpdate()) return 0;
+        return BetaUpdaterController.getInstance().getDownloadingProgress();
+    }
+
+    @Override
+    public File getDownloadedUpdateFile() {
+        if (!isCustomUpdate()) return null;
+        return BetaUpdaterController.getInstance().getDownloadedFile();
+    }
+
+    @Override
+    public IUpdateLayout takeUpdateLayout(Activity activity, ViewGroup sideMenuContainer) {
+        if (!isCustomUpdate()) return null;
+        return new UpdateLayout(activity, sideMenuContainer);
+    }
+
+    @Override
+    public boolean showCustomUpdateAppPopup(Context context, BetaUpdate update, int account) {
+        try {
+            (new UpdateAppAlertDialog(context, update, account)).show();
+        } catch (Exception e) {
+            FileLog.e(e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean checkApkInstallPermissions(final Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !ApplicationLoader.applicationContext.getPackageManager().canRequestPackageInstalls()) {
+            AlertsCreator.createApkRestrictedDialog(context, null).show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean openApkInstall(Activity activity, TLRPC.Document document) {
+        boolean exists = false;
+        try {
+            final String fileName = FileLoader.getAttachFileName(document);
+            final File f = FileLoader.getInstance(UserConfig.selectedAccount).getPathToAttach(document, true);
+            if (exists = f.exists()) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                if (Build.VERSION.SDK_INT >= 24) {
+                    intent.setDataAndType(FileProvider.getUriForFile(activity, ApplicationLoader.getApplicationId() + ".provider", f), "application/vnd.android.package-archive");
+                } else {
+                    intent.setDataAndType(Uri.fromFile(f), "application/vnd.android.package-archive");
+                }
+                try {
+                    activity.startActivityForResult(intent, 500);
+                } catch (Exception e) {
+                    FileLog.e(e);
+                }
+            }
+        } catch (Exception e) {
+            FileLog.e(e);
+        }
+        return exists;
+    }
 }
+
